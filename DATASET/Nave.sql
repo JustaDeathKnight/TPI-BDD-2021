c1df2682a7988790ea57a9a8ead71ed3a7258072
Acevedo Ariel <AcevedoAriel@outlook.com>
2022-03-16 00:43:28 -0300

    Add files via upload

---
+++ DATASET/Nave.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: srv-bbdd.local    Database: TPIDEMO
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `Nave`
+--
+
+LOCK TABLES `Nave` WRITE;
+/*!40000 ALTER TABLE `Nave` DISABLE KEYS */;
+INSERT INTO `Nave` VALUES ('A','1165517337','Property department','Vanity Fair','Apple Inc.'),('A','1613643118','Logistics','Robinson Crusoe','DynCorp'),('A','1775411958','Legal and public relation department','Tess of the d\'Urbervilles','DynCorp'),('A','1928584992','Personnel Department','Les MisÃ©rables','Boeing'),('B','1633627658','International Operations','The Flowers of Evil','Metro Cash&Carry'),('B','1888519394','Non-Store Operations','War and Peace','Global Print'),('B','2130478654','Non-Store Operations','Gulliver\'s Travels','Areon Impex'),('C','1118075597','Advertisement department','Nineteen Eighty Four','DynCorp'),('C','1271811096','Advertisement department','The Sound and the Fury','Boeing'),('C','1316787792','Domestic operations','The Sun Also Rises','Demaco');
+/*!40000 ALTER TABLE `Nave` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-20  0:51:06
